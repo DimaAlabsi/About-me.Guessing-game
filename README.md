7270b08e98d0602ef753a43d93b591ab1d79fce2
DimaAlabsi <dema.yaser8@gmail.com>
2021-07-27 18:47:15 +0300

    adding new questions to the user

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# AboutMe.Guessing-game
+# **AboutMe.Guessing-game**
+![guessing-game](https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTj274qgpAPsSn5LSOhGgeSccq-MMWOYcvrVA&usqp=CAU) 
+
+ + ## Describe my  project with the new features I am adding :
+
+#### *My webpage that quizzes the user on fun facts gives the user some basic information about who I am and where Icame from also gives the user informayion about my education, something love to do and etc..*
+
+
+
+![about me](https://st2.depositphotos.com/1032577/5312/i/950/depositphotos_53128333-stock-photo-about-me-sign.jpg)
+
+
+
+
+
+
--- js/app.js
+++ js/app.js
@@ -2,55 +2,56 @@
 
 let username = prompt('what is your name, dear?');
 
-alert(' Welcome to my website ..'+ username );
+alert(' Welcome to my website ..' + username);
 
 
 
 let myAge = prompt('Do you think my age less than 25 yaers ? yes/y or no/n ').toLowerCase();
 let counter = 0;
 
-while( myAge !== 'yes' &&  myAge !== 'y' &&  myAge !== 'no' &&  myAge !== 'n') {
-   myAge = prompt('Please enter yes or no');}
+while (myAge !== 'yes' && myAge !== 'y' && myAge !== 'no' && myAge !== 'n') {
+  myAge = prompt('Please enter yes or no');
+}
 
 
 
 
-switch(myAge) {
-case 'yes' , 'y' :
-//   console.log('yes,the user gussed');
-  alert('Bravo, you guess it ..'+ username + ' . I am 24 years old.');
-  counter++;
-  break;
-case 'no' ,'n':
-  //    console.log('no,the user don't guess');
-  alert('oh, I am 24 years old');
+switch (myAge) {
+  case 'yes', 'y':
+    //   console.log('yes,the user gussed');
+    alert('Bravo, you guess it ..' + username + ' . I am 24 years old.');
+    counter++;
+    break;
+  case 'no', 'n':
+    //    console.log('no,the user don't guess');
+    alert('oh, I am 24 years old');
 
 
-  break;
+    break;
 
 
 }
- 
+
 
 let lifePlace = prompt('Do you think I live in Jordan ? yes or no ').toLowerCase();
-while( lifePlace !== 'yes' &&  lifePlace !== 'y' &&  lifePlace !== 'no' &&  lifePlace !== 'n') {
-   lifePlace = prompt('Please enter yes or no');
+while (lifePlace !== 'yes' && lifePlace !== 'y' && lifePlace !== 'no' && lifePlace !== 'n') {
+  lifePlace = prompt('Please enter yes or no');
 
 }
-switch(lifePlace) {
-case 'yes', 'y':
-//   console.log('yes,the user gussed');
+switch (lifePlace) {
+  case 'yes', 'y':
+    //   console.log('yes,the user gussed');
 
-  alert('Bravo, you guess it.. '+ username + ' . I live in Amman.');
-   counter++;
-  break;
-case 'no', 'n':
-  //    console.log('no,the user don't guess');
-  alert ( 'oh, I live in Amman_Jordan');
+    alert('Bravo, you guess it.. ' + username + ' . I live in Amman.');
+    counter++;
+    break;
+  case 'no', 'n':
+    //    console.log('no,the user don't guess');
+    alert('oh, I live in Amman_Jordan');
 
 
 
-  break;
+    break;
 
 
 }
@@ -59,49 +60,49 @@ case 'no', 'n':
 
 
 let myInterest = prompt('Do you think I am intrested in Programming and software development ').toLowerCase();
-while( myInterest !== 'yes' &&  myInterest !== 'y' && myInterest !== 'no' &&  lifePlace !== 'n') {
+while (myInterest !== 'yes' && myInterest !== 'y' && myInterest !== 'no' && lifePlace !== 'n') {
   myInterest = prompt('Please enter yes or no');
 
 }
-switch(myInterest) {
-case 'yes', 'y':
-//   console.log('yes,the user gussed');
+switch (myInterest) {
+  case 'yes', 'y':
+    //   console.log('yes,the user gussed');
 
-  alert('Bravo, you guess it..'+ username + ' . I am so intersted in this field.');
-  
-  counter++;
-  break;
-case 'no','n':
-  //    console.log('no,the user don't guess');
- alert('oh, why not it is a very interesting field');
+    alert('Bravo, you guess it..' + username + ' . I am so intersted in this field.');
+
+    counter++;
+    break;
+  case 'no', 'n':
+    //    console.log('no,the user don't guess');
+    alert('oh, why not it is a very interesting field');
 
 
 
-  break;
+    break;
 
 
 
 }
 
 let aboutMovies = prompt('Do you think I like watching movies ').toLowerCase();
-while( aboutMovies !== 'yes' && aboutMovies !== 'y' && aboutMovies !== 'no' &&  lifePlace !== 'n') {
- aboutMovies = prompt('Please enter yes or no');
+while (aboutMovies !== 'yes' && aboutMovies !== 'y' && aboutMovies !== 'no' && lifePlace !== 'n') {
+  aboutMovies = prompt('Please enter yes or no');
 
 }
-switch(aboutMovies) {
-case 'yes','y':
-//   console.log('yes,the user gussed');
+switch (aboutMovies) {
+  case 'yes', 'y':
+    //   console.log('yes,the user gussed');
 
-  alert('Bravo, you guess it ..'+ username + ' . I like watching movies very much');
-  
-  counter++;
-  break;
-case 'no','n':
-  //    console.log('no,the user don't guess');
-  alert('oh, it is the most thing I love to do after listening to others experiences');
+    alert('Bravo, you guess it ..' + username + ' . I like watching movies very much');
+
+    counter++;
+    break;
+  case 'no', 'n':
+    //    console.log('no,the user don't guess');
+    alert('oh, it is the most thing I love to do after listening to others experiences');
 
 
-  break;
+    break;
 
 
 
@@ -109,34 +110,89 @@ case 'no','n':
 
 
 let favMovie = prompt('Do you think I love watching horror movies? ').toLowerCase();
-while( favMovie !== 'yes' && favMovie !== 'y' && favMovie !== 'no' &&  lifePlace !== 'n') {
- favMovie = prompt('Please enter yes or no');
+while (favMovie !== 'yes' && favMovie !== 'y' && favMovie !== 'no' && lifePlace !== 'n') {
+  favMovie = prompt('Please enter yes or no');
 }
-switch(favMovie) {
-case 'yes','y':
-//   console.log('no.the user donot guess');
+switch (favMovie) {
+  case 'yes', 'y':
+    //   console.log('no.the user donot guess');
 
-  alert('Oh, no it is impossible to watch this kind of movies');
-  
-  break;
-case 'no', 'n':
-  //    console.log('user guessed');
-  alert('yes dear, I do not never prefer this kind of movies .');
+    alert('Oh, no it is impossible to watch this kind of movies');
+
+    break;
+  case 'no', 'n':
+    //    console.log('user guessed');
+    alert('yes dear, I do not never prefer this kind of movies .');
+    counter++;
+
+    break;
+
+
+
+}
+
+
+
+
+let guessBirthady = Number(prompt(username + ' Can you guess my birthday month?'));
+
+// console.log (guessBirthady)
+for (let i = 0; i < 4; i++) {
+  if (guessBirthady === 8) { alert('oh you guess my birthday month');
   counter++;
+  break; }
+ 
+  
+  guessBirthady = (Numberprompt(username + ' Can you guess my birthday month?'));
 
-  break;
+// console.log(guessBirthady)
+  if ( guessBirthady < 8) { alert('try again ,it is too low'); }
+  guessBirthady = Number(prompt(username + ' Can you guess my birthday month?'));
 
-
+  // console.log(guessBirthady)
+  if ( guessBirthady > 8) { alert('try again ,it is too high')};
+  
 
 }
 
+  //  while( guessBirthady !== 8 && aboutMovies > 8) {
+  //     guessBirthady = prompt('try again ,it is too high');
+  //     while( guessBirthady !== 8 && aboutMovies < 8) {
+  //       guessBirthady = prompt('try again ,it is too low');
+  //     while( guessBirthady ===) {
+  //       guessBirthady = prompt( `oh ${username}, you guess my birthday month  `);
+  //        counter++;}
 
 
 
-alert(  username  +"  your score is  "+ counter)
+
+
+alert ('my birthady month is 8')
 
 
 
+let favColor = ['pink', 'purple','white','blue','black'];
+let guessColor = prompt(username + ' Can you guess my favourite color?');
+// console.log (guessColor)
+for (let x = 0; x < 6; x++) {
+   if (guessColor=== favColor[2]) { alert('oh you guess my favourite color');
+ counter++;
+   break; }
+ 
+  
+ 
 
-alert('Hope you have a great time at my website ^_^ '+ username );
+  guessColor = prompt(username + ' Can you guess my favourite color?');
+  // console.log (guessColor)
+
+//   if ( guessColor === favColor [3] ) { alert('try again ,it is not my favourite one'); }
+  
+while (guessColor !== favColor[2]) {alert('try again , it is not my favourite one')}
+ // console.log (guessColor)
+
+}
+
+alert('your score is ' + counter)
+
+alert(' Hope you have a great time at my website ^_^ ' + username);
 
