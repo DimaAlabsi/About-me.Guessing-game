e95408681e1bd304242aa40d97a5bc2ca85a979c
DimaAlabsi <86602837+DimaAlabsi@users.noreply.github.com>
2021-07-29 09:08:40 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -198,7 +198,7 @@ function guessColor(){
         break outerloop;
       }
      else { alert('try again ,it is not my favourite one');
-    guessColor= prompt(username+ 'can you guess favourite color');
+    guessColor= prompt(username+ 'can you guess favourite color?');
           }
     if (x === 5){alert ('sorry. y0u do not guess it');}
     
