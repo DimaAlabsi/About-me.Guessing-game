b41c61e2cd25449d6047cd161a508bcbcc7c7ffe
DimaAlabsi <86602837+DimaAlabsi@users.noreply.github.com>
2021-07-29 09:13:58 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -216,8 +216,5 @@ guessColor();
 
 // console.log (guessColor)
 alert ('My favourite colors are: white, green, pink, purple colors');
-
-
-
 alert('your score is ' + counter);
-alert(' Hope you have a great time at my website ^_^ ')
+alert(' Hope you have a great time at my website ^_^ ');
